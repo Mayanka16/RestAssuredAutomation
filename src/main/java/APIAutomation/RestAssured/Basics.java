35468ec7c19eed549e8cc65cbac9c95252334876
Mayanka Sao <mayankasao@gmail.com>
2024-07-05 16:00:48 +0530

    parsing the json response

--- src/main/java/APIAutomation/RestAssured/Basics.java
+++ src/main/java/APIAutomation/RestAssured/Basics.java
@@ -1,6 +1,8 @@
 package APIAutomation.RestAssured;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -12,11 +14,21 @@ public class Basics {
 		// given() = all input details
 		// when() = Submit the API - resource, http method
 		// Then = validate the response
-		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-				.body(Payload.addPlace()).when().post("maps/api/place/add/json").then().log().all().assertThat()
-				.statusCode(200).body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)");
+		 RestAssured.baseURI = "https://rahulshettyacademy.com";
+		 String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+				.body(Payload.addPlace()).when().post("maps/api/place/add/json").then().assertThat()
+				.statusCode(200).body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract()
+				.response().asString();
+		 
+		 System.out.println("*********************************************************************************************************");
+		 
+		 System.out.println("The response is : "+ response);
+		 
+		 JsonPath js = new JsonPath(response);
+		 
+		 
 
 	}
+	
 
 }
