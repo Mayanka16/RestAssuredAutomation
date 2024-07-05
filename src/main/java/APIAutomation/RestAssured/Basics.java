d9c212ecc93f781b2b1571b31fc805f40803961c
Mayanka Sao <mayankasao@gmail.com>
2024-07-05 17:38:47 +0530

    added reusableMethod class to add JsonPath file

--- src/main/java/APIAutomation/RestAssured/Basics.java
+++ src/main/java/APIAutomation/RestAssured/Basics.java
@@ -6,7 +6,10 @@ import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import org.testng.Assert;
+
 import APIAutomation.file.Payload;
+import APIAutomation.file.ReusableMethods;
 
 public class Basics {
 
@@ -14,18 +17,15 @@ public class Basics {
 		// given() = all input details
 		// when() = Submit the API - resource, http method
 		// Then = validate the response
-		
-		
-	
+
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		
-		//POST 
+
+		// POST
 		System.out.println("-----------------POST REQUEST STARTED---------------");
 		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
 				.body(Payload.addPlace()).when().post("maps/api/place/add/json").then().assertThat().statusCode(200)
 				.body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().response()
 				.asString();
-
 
 		System.out.println("The response is : " + response);
 
@@ -36,29 +36,33 @@ public class Basics {
 
 		String placeID = js.getString("place_id");
 		System.out.println("Place id from response is : " + placeID);
-		
-		
-		
+
 		System.out.println("---------------POST REQUEST ENDED---------------");
 
 		// Update address
 		System.out.println("---------------PUT REQUEST STARTED---------------");
-		
+
 		String newAddress = "PSOTS, Bangalore";
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-				.body("{\n" + "\"place_id\":\"" + placeID + "\",\n" + "\"address\":\""+newAddress+"\",\n"
+				.body("{\n" + "\"place_id\":\"" + placeID + "\",\n" + "\"address\":\"" + newAddress + "\",\n"
 						+ "\"key\":\"qaclick123\"\n" + "}\n" + " ")
 				.when().put("/maps/api/place/update/json").then().assertThat().log().all().statusCode(200)
 				.body("msg", equalTo("Address successfully updated"));
-		
+
 		System.out.println("---------------PUT REQUEST ENDED---------------");
 
 		// Get Response
-		
+
 		System.out.println("---------------GET REQUEST STARTED---------------");
-		given().log().all().queryParam("key", "qaclick123").queryParam("place_id", placeID).when()
-				.get("/maps/api/place/get/json").then().assertThat().log().all().statusCode(200)
-				.body("address", equalTo(newAddress));
+		String getPlaceResponse = given().log().all().queryParam("key", "qaclick123").queryParam("place_id", placeID)
+				.when().get("/maps/api/place/get/json").then().assertThat().log().all().statusCode(200).extract()
+				.response().asString();
+
+		System.out.println("The response is : " + getPlaceResponse);
+		JsonPath js1 = ReusableMethods.rawToJason(getPlaceResponse);
+		String actualAddress = js1.getString("address");
+		Assert.assertEquals(actualAddress, newAddress);
+
 		System.out.println("---------------GET REQUEST ENDED---------------");
 	}
 
