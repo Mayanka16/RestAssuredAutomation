0d9d9c4c25c47ebd5821dd7251d76ab9fddbf0e1
Mayanka Sao <mayankasao@gmail.com>
2024-07-10 14:34:00 +0530

    Added mock API data for validations

---
+++ src/main/java/APIAutomation/file/ReusableMethods.java
@@ -0,0 +1,14 @@
+package APIAutomation.file;
+
+import io.restassured.path.json.JsonPath;
+
+public class ReusableMethods {
+	
+	public static JsonPath rawToJason(String response) {
+		JsonPath js1 = new JsonPath(response);
+		return js1;
+		
+		
+	}
+
+}
